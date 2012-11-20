054f1db8f532841e896e9a2975bfa1ec876f0934
Mogria <m0gr14@gmail.com>
2012-11-21 00:35:30 +0100

    added functions to dump string to file or stdout

--- include/mcstring/dump.h
+++ include/mcstring/dump.h
@@ -4,5 +4,7 @@
 #include "mcstring.h"
 
 char *string_hexdump(mcstring *string);
+void file_hexdump(FILE *file, mcstring *string);
+void print_hexdump(mcstring *string);
 
 #endif /* MCSTRING_DUMP_H */
--- src/dump.c
+++ src/dump.c
@@ -6,10 +6,17 @@
 #define HEX_SEGMENT_SIZE (sizeof(mcchar) * 2)
 #define SEGMENT_SIZE (HEX_SEGMENT_SIZE + 1)
 
+static char *hexdump_format() {
+  static char *format[10] = {};
+  if(format[0] == '\0') {
+    snprintf(format, 10, "%%0%iX", HEX_SEGMENT_SIZE);
+  }
+  return format;
+}
+
 char *string_hexdump(mcstring *string) {
   char *ret = malloc(SEGMENT_SIZE * string->size);
-  char sprintf_format[10];
-  snprintf(sprintf_format, 10, "%%0%iX", HEX_SEGMENT_SIZE);
+  char *sprintf_format = hexdump_format();
   int x;
   for(x = 0; x < string->size; x++) {
     snprintf(ret + x * SEGMENT_SIZE,
@@ -21,4 +28,17 @@ char *string_hexdump(mcstring *string) {
   ret[x * SEGMENT_SIZE - 1] = '\0';
 
   return ret;
+}
+
+void file_hexdump(FILE *file, mcstring *string) {
+  char *printf_format = hexdump_format();
+  int x;
+  for(x = 0; x < string->size; x++) {
+    fprintf(file, printf_format, string->data[x]);
+    fprintf(file, " ");
+  }
+}
+
+void print_hexdump(mcstring *string) {
+  return file_hexdump(stdio, string);
 }
